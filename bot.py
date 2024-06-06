7dab44815387e05da83e265dcfc943bce7ac0cb4
yishain11 <yishain11@walla.co.il>
2024-06-06 10:30:58 +0300

    added basic bot

---
+++ bot.py
@@ -0,0 +1,28 @@
+from telegram import Update, ForceReply, InlineKeyboardMarkup, InlineKeyboardButton, ParseMode
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext, CallbackQueryHandler
+
+import os
+token = os.environ["TELEGRAM_BOT_TOKEN"]
+
+def say_name(update, context):
+    update.message.reply_text('Hello! I am your bot.')
+
+def main() -> None:
+    updater = Updater(token)
+
+    # Get the dispatcher to register handlers
+    # Then, we register each handler and the conditions the update must meet to trigger it
+    dispatcher = updater.dispatcher
+
+    # Register commands
+    dispatcher.add_handler(CommandHandler("hi", say_name))
+
+    # Start the Bot
+    updater.start_polling()
+
+    # Run the bot until you press Ctrl-C
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
